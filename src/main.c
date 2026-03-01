d42a552e25dbcaf2934bed7da2eaea8c7f91dd2d
Kolin63 <theverylastgoodname@gmail.com>
2026-03-01 17:44:46 -0500

    Add cli arg to set start level for testing

--- src/main.c
+++ src/main.c
@@ -2,10 +2,21 @@
 #include <stdlib.h>
 
 #include "manager.h"
+#include "tile.h"
 
-int main() {
+int main(int argc, char** argv) {
   manager_init();
   atexit(manager_cleanup);
+  // this is to help testing.
+  // USAGE: ./game [level number]
+  int start_level = 0;
+  if (argc >= 2) {
+    start_level = atoi(argv[1]);
+  }
+  manager_get_global()->current_level = start_level;
+  if (start_level != 0) {
+    tilemap_load_level(start_level);
+  }
   manager_run_game();
   manager_cleanup();
 
