edb627e02eead49a9e85ccfa46eb88687d704c98
Kolin63 <theverylastgoodname@gmail.com>
2026-03-01 19:32:38 -0500

    Remove unused includes

--- src/enemy.c
+++ src/enemy.c
@@ -2,7 +2,6 @@
 
 #include <math.h>
 #include <raymath.h>
-#include <stdlib.h>
 
 #include "manager.h"
 #include "tile.h"
--- src/tile.c
+++ src/tile.c
@@ -1,6 +1,5 @@
 #include "tile.h"
 
-#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
